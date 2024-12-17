a3464ad62f406f56262cce1844fbe4b67aa07061
razachikyan <razmikachikyan5@gmail.com>
2024-12-17 17:50:44 +0400

    BE change

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -6,19 +6,22 @@ class AuthController {
 
   async signup(req: Request, res: Response) {
     try {
-      const user = await authServices.signup(req.body);
-      res.status(201).json(user);
+      console.log('--------------', req.body);
+      
+      // const user = await authServices.signup(req.body);
+      // res.status(201).json(user);
     } catch (error) {
       console.log("Error creating user:", error);
       res.status(500).json(error);
     }
   }
-
+  
   async login(req: Request, res: Response) {
     try {
-      const { email, password } = req.body;
-      const user = await authServices.login(email, password);
-      res.status(200).json(user);
+      console.log('--------------', req.body);
+      // const { email, password } = req.body;
+      // const user = await authServices.login(email, password);
+      // res.status(200).json(user);
     } catch (error) {
       console.log("Error authenticating user:", error);
       res.status(500).json(error);
