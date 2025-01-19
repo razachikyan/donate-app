b249847f1321a309b5b5ccf7af00c0294e736972
razachikyan <razmikachikyan5@gmail.com>
2025-01-19 14:30:42 +0400

    fix

--- client/src/models/dtos/ItemTDO.ts
+++ client/src/models/dtos/ItemTDO.ts
@@ -7,7 +7,7 @@ export class ItemDTO extends BaseDTO {
     public address: string,
     public category: string,
     public condition: "new" | "good" | "used",
-    public status: "available" | "reserved" | "donated";,
+    public status: "available" | "reserved" | "donated",
     public donor_id: string,
     public image_url: string
   ) {
